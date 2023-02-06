8b046057084a7a978ec4f7748ba26f8f9d55510f
codygunton <codygunton@gmail.com>
2023-02-06 22:06:46 +0000

    Add alternative Foundry install scripts.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -eu
+
 git submodule init
 git submodule update
 cd barretenberg/cpp
@@ -5,3 +8,18 @@ cd ./srs_db
 ./download_ignition.sh 3
 ./download_ignition_lagrange.sh 12
 cd ../../../
+
+# Clean
+rm -rf broadcast cache out serve
+
+# Install foundry.
+. ./scripts/install_foundry.sh
+
+# Install
+forge install --no-commit
+
+# Ensure libraries are at the correct version
+git submodule update --init --recursive ./lib
+
+# Compile contracts
+forge build
