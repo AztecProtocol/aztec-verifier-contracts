3b82a4c4e72eb20012683898a1f6ecf214fc6dfc
LHerskind <lasse.herskind@gmail.com>
2023-02-28 17:32:08 +0000

    fix: Update test to have odd number of inputs

--- test/standard/TestBaseStandard.sol
+++ test/standard/TestBaseStandard.sol
@@ -26,40 +26,13 @@ contract TestBaseStandard is TestBase {
         bytes memory proofData = fuzzer.generate_proof();
         (bytes32[] memory publicInputs, bytes memory proof) = splitProof(proofData, PUBLIC_INPUT_COUNT);
 
+        // Invalidates the proof by adding 1 to one of the elements
         assembly {
             let where := add(add(proof, 0x20), mul(0x20, 2))
             mstore(where, add(where, 0x01))
         }
 
         vm.expectRevert(abi.encodeWithSelector(BaseStandardVerifier.PROOF_FAILURE.selector));
-        verifier.verify(proof, publicInputs);
-    }
-
-    function _testVerifierInvalidBn128G1() public {
-        // NOTE: This test only makes sense if we are doing a recursive proof.
-
-        _testVerifierInvalidBn128Component(0x40); // x0
-        _testVerifierInvalidBn128Component(0xc0); // y0
-        _testVerifierInvalidBn128Component(0x140); // x1
-        _testVerifierInvalidBn128Component(0x1c0); // y1
-    }
-
-    function _testVerifierInvalidBn128Component(uint256 _offset) internal {
-        bytes memory proofData = fuzzer.generate_proof();
-        (bytes32[] memory publicInputs, bytes memory proof) = splitProof(proofData, PUBLIC_INPUT_COUNT);
-
-        {
-            uint256 q = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
-
-            assembly {
-                mstore(add(proof, _offset), and(shr(0, q), 0x0fffffffffffffffff))
-                mstore(add(proof, add(_offset, 0x20)), and(shr(68, q), 0x0fffffffffffffffff))
-                mstore(add(proof, add(_offset, 0x40)), and(shr(136, q), 0x0fffffffffffffffff))
-                mstore(add(proof, add(_offset, 0x60)), and(shr(204, q), 0x0fffffffffffffffff))
-            }
-        }
-
-        vm.expectRevert(BaseStandardVerifier.PUBLIC_INPUT_INVALID_BN128_G1_POINT.selector);
         verifier.verify(proof, publicInputs);
     }
 
@@ -73,10 +46,5 @@ contract TestBaseStandard is TestBase {
 
         vm.expectRevert(BaseStandardVerifier.PUBLIC_INPUT_GE_P.selector);
         verifier.verify(proof, publicInputs);
-    }
-
-    function _testPublicRecursiveInputsNotInP(uint256 _offset) public {
-        // The last 68 bit limb in each point (except the last, as that is check in another testLastPublicInputNotInP)
-        revert("To be implemented");
     }
 }
